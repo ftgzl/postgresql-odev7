26bb07f2bbb5c23dda664846852f4cbef9562095
fuatguzel <fuatguzel754@gmail.com>
2021-09-19 23:51:18 +0300

    ✔

---
+++ Homework.sql
@@ -0,0 +1,18 @@
+-- Örnek 1
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating;
+
+-- Örnek 2
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+-- Örnek 3
+SELECT store_id ,COUNT(*) FROM customer
+GROUP BY store_id;
+
+-- Örnek 4
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
